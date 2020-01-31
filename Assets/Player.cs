eb8782579d70f258f9b7444babdfcd8099d5ac4a
Mike van der Velde <MikeJordy1999@gmail.com>
2020-01-31 21:56:19 +0100

    Intial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public GameObject[] tools;
+	public GameObject closestTool;
+	public GameObject heldTool;
+	public float lowestDistance = Mathf.Infinity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		tools = GameObject.FindGameObjectsWithTag("Tool");
+
+
+		//collider = gameObject.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+			if (heldTool != null) {
+				heldTool.GetComponent<Tool>().Drop(gameObject.transform.position);
+				heldTool = null;
+			} else {
+				if (lowestDistance < 0.3f) {
+					heldTool = closestTool.GetComponent<Tool>().PickUp();
+				}
+			}
+
+			
+		}
+
+
+
+    }
+
+	void FixedUpdate() {
+		
+		foreach (GameObject tool in tools) {
+			if (closestTool != null) {
+				float distanceToClosest = Vector2.Distance(gameObject.transform.position, closestTool.transform.position);
+				lowestDistance = distanceToClosest;
+			}
+
+
+			float distance = Vector2.Distance(gameObject.transform.position, tool.transform.position);
+			if (distance < lowestDistance) {
+				lowestDistance = distance;
+				closestTool = tool;
+			}
+		}
+	}
+}
---
+++ Assets/Tool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+	public enum ToolType {
+		TestTool,
+		TestTool2
+	}
+
+	public ToolType toolType;
+	private SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public GameObject PickUp() {
+		//renderer.enabled = false;
+		gameObject.SetActive(false);
+		Debug.Log("Picked up: " + toolType);
+		return gameObject;
+
+	}
+
+	public void Drop(Vector2 position) {
+		//renderer.enabled = true;
+		gameObject.SetActive(true);
+		gameObject.transform.position = position;
+	}
+}
